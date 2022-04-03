28a92552c5698e6e9631999f531f6eb74bbb8ecb
Muhammad Ridwan <mohridwan1707@gmail.com>
2022-04-04 05:17:46 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const weeklyTasksList = document.getElementById("weeklyTasksList");
+
+for (let i = 0; i < tasks.length; i++) {
+  weeklyTasksList.appendChild();
+}
+console.log(weeklyTasksList);
---
+++ data/task.js
@@ -0,0 +1,12 @@
+const tasks = [
+  {
+    id: 1,
+    name: "Version Control",
+  },
+  {
+    id: 2,
+    name: "Introduction UI/UX",
+  },
+];
+
+// module.export = tasks;
